f8247aeca30fccc65cfe16803c228bf900eb2f67
Keerthi Mani <carkeane.getz@gmail.com>
2020-02-07 16:50:02 -0500

    labels,data and background color from chart file

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,8 +2,48 @@ import React, { Component } from "react";
 import { Bar, line, Pie } from "react-chartjs-2";
 
 class Chart extends Component {
+  constructor() {
+    super(props);
+    this.state = {
+      chartDate: {
+        labels: [
+          "Boston",
+          "Worcester",
+          "Springfield",
+          "Lowell",
+          "Cambridge",
+          "New Bedford"
+        ],
+        datasets: [
+          {
+            label: "Population",
+            data: [617594, 181045, 153060, 106519, 105162, 95072],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.6)",
+              "rgba(54, 162, 235, 0.6)",
+              "rgba(255, 206, 86, 0.6)",
+              "rgba(75, 192, 192, 0.6)",
+              "rgba(153, 102, 255, 0.6)",
+              "rgba(255, 159, 64, 0.6)",
+              "rgba(255, 99, 132, 0.6)"
+            ]
+          }
+        ]
+      }
+    };
+  }
+
   render() {
-    return <div className="chart">CHART COMPONENT</div>;
+    return (
+      <div className="chart">
+        <Bar
+          data={data}
+          //   width={100}
+          //   height={50}
+          options={{ maintainAspectRatio: false }}
+        />
+      </div>
+    );
   }
 }
 export default Chart;
