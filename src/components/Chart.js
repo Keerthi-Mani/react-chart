926559e48c2a044a682eae799258df0845b72b53
Keerthi Mani <carkeane.getz@gmail.com>
2020-02-07 17:18:33 -0500

    render data using doughnut chart

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Bar, Line, Pie } from "react-chartjs-2";
+import { Bar, Line, Pie, Doughnut } from "react-chartjs-2";
 
 class Chart extends Component {
   constructor(props) {
@@ -90,6 +90,29 @@ class Chart extends Component {
         <br />
         {/* Pie Chart */}
         <Pie
+          data={this.state.chartData}
+          width={100}
+          height={50}
+          options={{
+            title: {
+              //   display: true,
+              display: this.props.displayTitle,
+              text: "Largest Cities In Massachusetts",
+              fontSize: 30
+            },
+            legend: {
+              //   display: true,
+              // position: "right"
+              display: this.props.displayLegend,
+              position: this.props.legendPosition
+            }
+          }}
+        />
+
+        <br />
+        <br />
+        {/* Doughnut Chart */}
+        <Doughnut
           data={this.state.chartData}
           width={100}
           height={50}
