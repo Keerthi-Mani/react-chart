2694667f8864148f9871af82cfebe1e0952decf8
Keerthi Mani <carkeane.getz@gmail.com>
2020-02-07 16:38:12 -0500

    import bar,pie n line chart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Chart from "./components/Chart";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Chart />
     </div>
   );
 }
---
+++ src/components/Chart.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { Bar, line, Pie } from "react-chartjs-2";
+
+class Chart extends Component {
+  render() {
+    return <div className="chart">CHART COMPONENT</div>;
+  }
+}
+export default Chart;
