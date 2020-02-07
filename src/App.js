2626683b2289983c685e72f314729a25ec5a20c5
Keerthi Mani <carkeane.getz@gmail.com>
2020-02-07 16:43:34 -0500

    import chart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import logo from "./logo.svg";
 import Chart from "./components/Chart";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </div>
       <Chart />
     </div>
   );
